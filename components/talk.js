c718dc371f1fb84ff414e42064a165cdcc22e7fc
Your Name <khon17801@gmail.com>
2022-11-28 21:47:51 +0700

    fix

--- components/talk.js
+++ components/talk.js
@@ -15,65 +15,51 @@ return (
   <main>
     <div ref={chatboxEl} />
   </main>
-)
+);
 const chatbox = session.createChatbox();
 chatbox.select(conversation);
 chatbox.mount(chatboxEl.current);
+// ...
+<section>
+  <button onClick={() => converse(jack)}>// ...</button>
+  <button onClick={() => converse(mary)}>// ...</button>
+</section>;
+useEffect(() => {
+  Talk.ready.then(() => {
+    const session = new Talk.Session({
+      appId: 'tWKLgMns',
+      me: currentUser,
+    });
+  });
+}, []);
+
+const currentUser = new Talk.User({
+  id: '1632',
+  name: 'Kate Smith',
+  email: 'katesmith@example.com',
+  photoUrl: 'kate.jpeg',
+  welcomeMessage: 'Hey there!',
+  role: 'default',
+});
+
+const conversation = session.getOrCreateConversation(
+  Talk.oneOnOneId(currentUser, otherUser)
+);
+conversation.setParticipant(currentUser);
+conversation.setParticipant(otherUser);
 function converse(user) {
   Talk.ready.then(() => {
     // ...
-    <section>
-    <button onClick={() => converse(jack)}>
-      // ...
-    </button>
-    <button onClick={() => converse(mary)}>
-      // ...
-    </button>
-  </section>      
-useEffect(() => {
-    Talk.ready.then(() => {
-      const session = new Talk.Session({
-        appId: 'tWKLgMns',
-        me: currentUser,
-      });
-    });
-  }, []);
- 
-    const currentUser = new Talk.User({
-    id: '1632',
-    name: 'Kate Smith',
-    email: 'katesmith@example.com',
-    photoUrl: 'kate.jpeg',
-    welcomeMessage: 'Hey there!',
-    role: 'default',
+    const otherUser = new Talk.User(user);
+    // ...
   });
-  
-  const conversation = session.getOrCreateConversation(
-    Talk.oneOnOneId(currentUser, otherUser)
-  );
-  conversation.setParticipant(currentUser);
-      conversation.setParticipant(otherUser);
-      function converse(user) {
-        Talk.ready.then(() => {
-          // ...
-          const otherUser = new Talk.User(user);
-          // ...
-        });
-      }
-  const otherUser = new Talk.User({
-    id: '1629',
-    name: 'Jack White',
-    email: 'jackwhite@example.com',
-    photoUrl: 'jack.jpeg',
-    welcomeMessage: 'Hi, how can I help?',
-    role: 'default',
-  });
-  // ...
-  const session = new Talk.Session({
-    
-      
-      
-   
-       
-      
-   
+}
+const otherUser = new Talk.User({
+  id: '1629',
+  name: 'Jack White',
+  email: 'jackwhite@example.com',
+  photoUrl: 'jack.jpeg',
+  welcomeMessage: 'Hi, how can I help?',
+  role: 'default',
+});
+// ...
