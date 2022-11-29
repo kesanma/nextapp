145742b83f2e581c86b34e67ea516666b0721ac4
Your Name <khon17801@gmail.com>
2022-11-29 11:09:44 +0700

    mess

--- pages/facbook.js
+++ pages/facbook.js
@@ -1,56 +1,12 @@
-import { useEffect } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-/**
- *
- */
-export function init() {
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute('page_id', '110501698558413');
-  chatbox.setAttribute('attribution', 'biz_inbox');
-
-  window.fbAsyncInit = function () {
-    FB.init({
-      xfbml: true,
-      version: 'v15.0',
-    });
-  };
-
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, 'script', 'facebook-jssdk');
-}
-export function cleanup() {
-  (function (d, id) {
-    var target = d.getElementById(id);
-    if (target) {
-      target.parentNode.removeChild(target);
-    }
-  })(document, 'facebook-jssdk');
-
-  delete window.FB;
-}
-
-export function Facebook1() {
-  useEffect(() => {
-    console.log('Facebook1');
-    init();
-
-    return () => {
-      cleanup();
-    };
-  }, []);
-
-  return (
-    <div>
-      <div id="fb-root"></div>
-
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
-    </div>
-  );
-}
+ReactDOM.render(
+  <MessengerCustomerChat
+    pageId="110501698558413"
+    appId="692692862512802"
+    htmlRef="https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js"
+  />,
+  document.getElementById('demo')
+);
