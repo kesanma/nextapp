6a4cfb4d25d2713e279ddebf355df76bfbf47f47
Your Name <khon17801@gmail.com>
2022-11-29 11:50:17 +0700

    update

--- pages/facbook.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-
-ReactDOM.render(
-  <MessengerCustomerChat pageId="110501698558413" appId="692692862512802" />,
-  document.getElementById('demo')
-);
