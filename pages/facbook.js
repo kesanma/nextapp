0457c0fe495a3869c5d2ae4c131a0e07f2e495f0
Your Name <khon17801@gmail.com>
2022-11-29 11:18:20 +0700

    update

--- pages/facbook.js
+++ pages/facbook.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 ReactDOM.render(
-  <MessengerCustomerChat
-    pageId="110501698558413"
-    appId="692692862512802"
-    htmlRef="https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js"
-  />,
+  <MessengerCustomerChat pageId="110501698558413" appId="692692862512802" />,
   document.getElementById('demo')
 );
