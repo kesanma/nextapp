ed8ddd97cc5b6f2ab2dc75e42260e0998083aeb2
Your Name <khon17801@gmail.com>
2022-12-01 06:53:06 +0700

    up

--- pages/_app.js
+++ pages/_app.js
@@ -40,25 +40,4 @@ function Auth({ children, adminOnly }) {
 
   return children;
 }
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return <Component {...pageProps} />;
-}
-
 export default MyApp;
