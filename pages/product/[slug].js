4ece8265291e533cf214707ec858ffabeb72d30a
Your Name <khon17801@gmail.com>
2022-12-01 07:51:17 +0700

    up

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -90,33 +90,7 @@ export default function ProductScreen(props) {
           >
             <LinkedinIcon size={32} round />
           </LinkedinShareButton>
-    <div id="fb-root"></div>
-
-
-    <div id="fb-customer-chat" class="fb-customerchat">
-    </div>
-
-    <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "110501698558413");
-      chatbox.setAttribute("attribution", "biz_inbox");
-    </script>
-
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v15.0'
-        })};
-
-      (function(d, s, id) {
-        js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    </script>
+          <div id="fb-root"></div>
         </div>
         <div>
           <div className="card p-5">
