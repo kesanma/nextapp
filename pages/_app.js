a98931cdab5285e3b92e1656088232a92c6e67a1
Your Name <khon17801@gmail.com>
2022-12-01 08:13:03 +0700

    up

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,7 @@ import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '../utils/Store';
 import { useRouter } from 'next/router';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-import { useEffect } from 'react';
-import * as ga from '../lib/ga';
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
